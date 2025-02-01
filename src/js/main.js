5fc5e11f3dad76c256ef9d956182a2cb8979be9a
Maya <maya.kolomoetc@yandex.ru>
2025-02-01 17:33:30 +0300

    start adaptiving

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,26 @@
 import '../css/style.css';
-const accordionLists = document.querySelectorAll('.accordion-list');
 
+document.addEventListener('click', burgerInit)
+
+    function burgerInit(e) {
+
+        const burgerIcon = e.target.closest('.burger-icon')
+        const burgerNavLink = e.target.closest('.nav__link')
+
+        if (!burgerIcon && !burgerNavLink) return
+        if (document.documentElement.clientWidth > 900) return
+
+        if (!document.body.classList.contains('body--opened-menu')) {
+            document.body.classList.add('body--opened-menu')
+        } else {
+            document.body.classList.remove('body--opened-menu')
+        }
+
+    }
+
+
+		const accordionLists = document.querySelectorAll('.accordion-list');
+		
 accordionLists.forEach(accordionList => {
     // Найти первый элемент списка
     const firstItem = accordionList.querySelector('.accordion-list__item');
